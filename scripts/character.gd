e7f46d82a41b1cb28fa5e85b14300e9aec074f24
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2019-11-11 23:37:07 +0100

    adds character movment inputmappings. adds jumping mechanism for character #2 - Character movement/physics

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,7 +4,21 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-const SPEED = 200
+const SPEED = 10000
+var jump_speed = 0
+
+enum JumpStates {
+	NONE,
+	JUMPING,
+	LANDING
+}
+
+
+var jump_state = JumpStates.NONE
+
+func is_jumping():
+	return jump_state != JumpStates.NONE
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,17 +32,44 @@ func _ready():
 
 
 func _physics_process(delta):
+	handle_jump_state()
+	
 	var movement_direction = Vector2()
 	
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		movement_direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		movement_direction.y -= 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		movement_direction.x -= 1
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		movement_direction.x += 1
 	
-	move_and_slide(movement_direction * SPEED)
+	move_and_slide(movement_direction * SPEED * delta)
+	
+	
+	
+	
 	
 
+func handle_jump_state():
+	var sprite = get_node("sprite")
+	var collision_shape = get_node("collision_shape")
+	if (jump_speed != 0):
+		sprite.move_local_y(-jump_speed)
+	
+	if (sprite.position.y < 0):
+		jump_speed -= 1
+	elif jump_speed != 0:
+		jump_speed = 0
+		
+		sprite.position.y = 0
+		set_collision_mask_bit(0, true)
+	
+	if (Input.is_action_pressed("jump") and sprite.position.y == 0):
+		# start jump
+		#jump_state = JumpStates.JUMPING
+		set_collision_mask_bit(0, false)
+		jump_speed = 10
+		print("jump")
+	
