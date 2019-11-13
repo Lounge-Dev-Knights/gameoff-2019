2386438aced257489d05ed6908cf35756aaa32d1
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2019-11-13 23:46:55 +0100

    adds comments to character code

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,24 +1,23 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# maximal movement speed
+const MAX_SPEED = 10000
 
-const SPEED = 10000
+# current jumping speed
 var jump_speed = 0
 
-enum JumpStates {
-	NONE,
-	JUMPING,
-	LANDING
-}
+# enum JumpStates {
+# 	NONE,
+# 	JUMPING,
+# 	LANDING
+# }
 
 
-var jump_state = JumpStates.NONE
+# var jump_state = JumpStates.NONE
 
-func is_jumping():
-	return jump_state != JumpStates.NONE
-	
+# func is_jumping():
+# 	return jump_state != JumpStates.NONE
+# 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,8 +31,10 @@ func _ready():
 
 
 func _physics_process(delta):
+	# handle jumping state or new jump action
 	handle_jump_state()
 	
+	# evaluate and apply movement direction
 	var movement_direction = Vector2()
 	
 	if Input.is_action_pressed("move_down"):
@@ -45,31 +46,40 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		movement_direction.x += 1
 	
-	move_and_slide(movement_direction * SPEED * delta)
-	
-	
-	
+	# TODO acceleration and deceleration
+	move_and_slide(movement_direction.normalized() * MAX_SPEED * delta)
 	
 	
 
+
 func handle_jump_state():
+	"""
+	handle currente jumping state
+	
+	moves the sprite for vizualation
+	disables collision while jumping
+	"""
+	
 	var sprite = get_node("sprite")
 	var collision_shape = get_node("collision_shape")
+	
 	if (jump_speed != 0):
+		# move sprite according to current jumping speed
 		sprite.move_local_y(-jump_speed)
 	
 	if (sprite.position.y < 0):
+		# decelerate jumping speed into falling
 		jump_speed -= 1
 	elif jump_speed != 0:
+		# reset sprite position and jumping speed when jump has ended
 		jump_speed = 0
-		
 		sprite.position.y = 0
 		set_collision_mask_bit(0, true)
 	
 	if (Input.is_action_pressed("jump") and sprite.position.y == 0):
 		# start jump
-		#jump_state = JumpStates.JUMPING
+		# disable collision
 		set_collision_mask_bit(0, false)
+		# set jumping speed
 		jump_speed = 10
-		print("jump")
 	
